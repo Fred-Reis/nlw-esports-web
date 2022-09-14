759a2e0f1b774af3ac04a8d0f049778c515246dd
frederico lopes <fredericolopes@fredericos-MacBook-Pro.local>
2022-09-13 23:32:09 -0300

    tailwind added landingpage finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,109 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+import "./styles/main.css";
+
+import logo from "./assets/logo.svg";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 1.png" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 2.png" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 3.png" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 4.png" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 5.png" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image 6.png" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden">
+        <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="font-black text-white text-2xl">
+              Não encontrou seu duo?
+            </strong>
+
+            <span className="text-zinc-400 text-sm block">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+
+          <button className="px-4 py-3 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anuncio
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
